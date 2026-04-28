052f542c5522d840314c57e14cd3aa8a81edbf32
Lalithya-lab <lalithya.yentrapati@gmail.com>
2026-04-28 16:43:36 +0530

    CurrentWeather is updated

--- src/components/CurrentWeather.vue
+++ src/components/CurrentWeather.vue
@@ -6,14 +6,14 @@
         <div class="col-md-6 mb-3">
             <div class="border rounded p-3">
                 <h5><span class="badge bg-info text-dark">Temperature</span></h5>
-                <h2>{{ weather.current.temperature_2m }}</h2>
+                <h2>{{ weather.current.temperature_2m }} °C</h2>
             </div>
         </div>
 
         <div class="col-md-6 mb-3">
             <div class="border rounded p-3">
                 <h5><span class="badge bg-warning text-white">Wind</span></h5>
-                <h2>{{ weather.current.wind_speed_10m }}</h2>
+                <h2>{{ weather.current.wind_speed_10m }} km/h</h2>
             </div>
         </div>
     </div>
